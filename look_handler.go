4c1811e239a66291d55d8631a90085fb7ee59f7f
matteo-gildone <matteo.gildone@springernature.com>
2026-04-10 07:40:43 +0100

    feat: add LookHandler

--- look_handler.go
+++ look_handler.go
@@ -2,6 +2,8 @@ package goforth
 
 import "fmt"
 
+// LookHandler prints the description and exits of the player's current room.
+// Register it as "look" in a CommandRegistry.
 func LookHandler(args []string, g *Game) error {
 	currentRoom, ok := g.World.RoomByID(g.Player.CurrentRoom())
 	if !ok {
